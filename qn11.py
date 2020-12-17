00c024a67bc6b876ebd1696ab643e0ad03a1d2ce
Meeraprasad1104 <74696239+Meeraprasad1104@users.noreply.github.com>
2020-12-17 20:55:08 +0530

    Add files via upload

---
+++ qn11.py
@@ -0,0 +1,6 @@
+n=input("enter the word=")
+value=ord(n)
+print("ordinary value=",value)
+
+
+
---
+++ qn12.py
@@ -0,0 +1,6 @@
+n=input("comma seperated numbers=")
+l=n.split()
+print("list is=",l)
+t=tuple(l)
+print("tuple is=",t)
+
