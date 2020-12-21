daffe1a5962e71a08058d6d65cf0ea75a73b96b5
Meeraprasad1104 <74696239+Meeraprasad1104@users.noreply.github.com>
2020-12-21 12:59:00 +0530

    Add files via upload

---
+++ qn16.py
@@ -0,0 +1,5 @@
+n=input('enter the string=')
+c=n[0]
+n=n.replace(c,'$')
+s=c+n[1:]
+print(s)
---
+++ qn17.py
@@ -0,0 +1,8 @@
+a=input("enter the first string=")
+b=input("enter the second string=")
+print("single string=",a,"",b)
+t=a
+a=b
+b=t
+print("swaping first string=",a)
+print("swaping second string=",b)
