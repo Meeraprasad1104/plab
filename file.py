9c7d12b5665db8ff8b81e27d1caa07e50d2e3b8f
Meeraprasad1104 <74696239+Meeraprasad1104@users.noreply.github.com>
2021-02-17 11:47:42 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,11 @@
+f=open('oe.txt','r')
+f1=open('o.txt','w')
+f2=open('e.txt','w')
+for i in map(int,f.readline().split()):
+    if  i%2 !=0:
+        f1.write(str(i)+' ')
+    else:
+        f2.write(str(i)+' ')
+f.close()
+f1.close()
+f2.close()
